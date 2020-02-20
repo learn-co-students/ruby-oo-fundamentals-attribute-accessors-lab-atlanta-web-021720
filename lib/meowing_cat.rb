870724cb1a53ad1738e1e526c794cf58b1164b77
github username <github email address>
2020-02-19 22:37:34 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
 ## code your solution here. 
+
+require "pry"
+class Cat 
+  def name 
+    @name 
+  end 
+attr_accessor :name 
+                    #   Write a method, .meow, that outputs "meow!" to the terminal 
+                    #using the puts method when called on an instance of Cat.
+  def meow
+    puts "meow!"
+  end 
+end
